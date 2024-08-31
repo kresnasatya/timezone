7c6ee95354092f38c91b3e901e1f6670ff5d06fb
Kresna Satya <kresnasatya72@gmail.com>
2024-08-31 09:31:06 +0800

    update Timezone component

--- src/lib/Timezone.svelte
+++ src/lib/Timezone.svelte
@@ -1,12 +1,26 @@
 <script>
     import { onInterval } from "./utils";
     import { getTZName, getFullDate, getTime } from "./timezone";
+	import Sun from "./Sun.svelte";
+	import Moon from "./Moon.svelte";
     export let timeZone;
 
     /**
 	 * @param {string} timeZone
 	 */
-     function getTimeClass(timeZone) {
+    function getIcon(timeZone) {
+        const hours = parseInt(new Date().toLocaleString('en-US', { timeZone, hour: '2-digit', hour12: false }));
+        if (hours >= 6 && hours < 18) {
+            return 'sun';
+        } else {
+            return 'moon';
+        }
+    }
+
+    /**
+	 * @param {string} timeZone
+	 */
+     function getIconTime(timeZone) {
         const hours = parseInt(new Date().toLocaleString('en-US', { timeZone, hour: '2-digit', hour12: false }));
         if (hours >= 6 && hours < 12) {
             return 'morning';
@@ -25,15 +39,23 @@
     }, 1000);
 </script>
 
-<div style="width: 100%; border: 1px solid gray;" class="rounded-md timezone">
-    <p class="font-medium" style="font-size: 1.25rem; margin: 0; padding: 0;">{timeZone.name} <span class="{getTimeClass(timeZone.tz)} inline-block p-2 rounded-md font-medium">{getTimeClass(timeZone.tz)}</span></p>
-    <div style="display: flex; justify-content: space-between; gap: 4px;">
+<div style="width: 100%; border: 1px solid gray;" 
+    class="rounded-md timezone { (getIcon(timeZone.tz) === 'sun') ? 'daytime' : 'nighttime' }">
+    <p class="font-medium" style="font-size: 1.25rem; margin: 0; padding: 0;">
+        {#if getIcon(timeZone.tz) === 'sun'}
+            <Sun time={getIconTime(timeZone.tz)}/>
+        {:else}
+            <Moon time={getIconTime(timeZone.tz)} />
+        {/if}
+        {timeZone.name}
+    </p>
+    <div style="display: flex; justify-content: space-between; gap: 1rem;">
         <div>
             <p>{timeZone.tz}</p>
-            <p style="color: gray;">{getTZName(timeZone.tz)}</p>
+            <p>{getTZName(timeZone.tz)}</p>
         </div>
         <div>
-            <p style="color: gray;">{getFullDate(timeZone.tz, currentTime)}</p>
+            <p>{getFullDate(timeZone.tz, currentTime)}</p>
         </div>
         <div>
             <p>{getTime(timeZone.tz, currentTime)}</p>
@@ -55,10 +77,6 @@
         font-weight: 500;
     }
 
-    .inline-block {
-        display: inline-block;
-    }
-
     /* Time utility class */
     .timezone {
         padding: 1rem;
@@ -73,25 +91,5 @@
     .nighttime {
         background-color: #2c3e50; /* Dark blue for night */
         color: #fff;
-    }
-
-    .morning {
-        background-color: #FFF9C4; /* Light Yellow */
-        color: #333333; /* Dark Gray */
-    }
-
-    .afternoon {
-        background-color: #FFEB3B; /* Bright Yellow */
-        color: #212121; /* Darker Gray */
-    }
-    
-    .evening {
-        background-color: #4b0082; /* Indigo for evening */
-        color: #dda0dd; /* Purple for evening */
-    }
-
-    .night {
-        background-color: #000033; /* Very dark blue for night */
-        color: #ffffff; /* White for night text */
     }
 </style>
