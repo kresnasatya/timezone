31bcce907bd5a483337e82af35c8e81b74e28c19
Kresna Satya <kresnasatya72@gmail.com>
2024-08-31 15:49:57 +0800

    load svg in svelte efficiently

--- src/lib/Timezone.svelte
+++ src/lib/Timezone.svelte
@@ -1,95 +1,96 @@
 <script>
-    import { onInterval } from "./utils";
-    import { getTZName, getFullDate, getTime } from "./timezone";
-	import Sun from "./Sun.svelte";
-	import Moon from "./Moon.svelte";
-    export let timeZone;
+	import { onInterval } from './utils';
+	import { getTZName, getFullDate, getTime } from './timezone';
+	import Icon from './Icon.svelte';
+	export let timeZone;
 
-    /**
+	/**
 	 * @param {string} timeZone
 	 */
-    function getIcon(timeZone) {
-        const hours = parseInt(new Date().toLocaleString('en-US', { timeZone, hour: '2-digit', hour12: false }));
-        if (hours >= 6 && hours < 18) {
-            return 'sun';
-        } else {
-            return 'moon';
-        }
-    }
+	function getIconName(timeZone) {
+		const hours = parseInt(
+			new Date().toLocaleString('en-US', { timeZone, hour: '2-digit', hour12: false })
+		);
+		if (hours >= 6 && hours < 18) {
+			return 'sun';
+		} else {
+			return 'moon';
+		}
+	}
 
-    /**
+	/**
 	 * @param {string} timeZone
 	 */
-     function getIconTime(timeZone) {
-        const hours = parseInt(new Date().toLocaleString('en-US', { timeZone, hour: '2-digit', hour12: false }));
-        if (hours >= 6 && hours < 12) {
-            return 'morning';
-        } else if (hours >= 12 && hours < 18) {
-            return 'afternoon';
-        } else if (hours >= 18 && hours < 21) {
-            return 'evening';
-        } else {
-            return 'night';
-        }
-    }
+	function getIconTime(timeZone) {
+		const hours = parseInt(
+			new Date().toLocaleString('en-US', { timeZone, hour: '2-digit', hour12: false })
+		);
+		if (hours >= 6 && hours < 12) {
+			return 'morning';
+		} else if (hours >= 12 && hours < 18) {
+			return 'afternoon';
+		} else if (hours >= 18 && hours < 21) {
+			return 'evening';
+		} else {
+			return 'night';
+		}
+	}
 
-    let currentTime = Date.now();
-    onInterval(() => {
-        currentTime = Date.now();
-    }, 1000);
+	let currentTime = Date.now();
+	onInterval(() => {
+		currentTime = Date.now();
+	}, 1000);
 </script>
 
-<div style="width: 100%; border: 1px solid gray;" 
-    class="rounded-md timezone { (getIcon(timeZone.tz) === 'sun') ? 'daytime' : 'nighttime' }">
-    <p class="font-medium" style="font-size: 1.25rem; margin: 0; padding: 0;">
-        {#if getIcon(timeZone.tz) === 'sun'}
-            <Sun time={getIconTime(timeZone.tz)}/>
-        {:else}
-            <Moon time={getIconTime(timeZone.tz)} />
-        {/if}
-        {timeZone.name}
-    </p>
-    <div style="display: flex; justify-content: space-between; gap: 1rem;">
-        <div>
-            <p>{timeZone.tz}</p>
-            <p>{getTZName(timeZone.tz)}</p>
-        </div>
-        <div>
-            <p>{getFullDate(timeZone.tz, currentTime)}</p>
-        </div>
-        <div>
-            <p>{getTime(timeZone.tz, currentTime)}</p>
-        </div>
-    </div>
+<div
+	style="width: 100%; border: 1px solid gray;"
+	class="rounded-md timezone {getIconName(timeZone.tz) === 'sun' ? 'daytime' : 'nighttime'}"
+>
+	<p class="font-medium" style="font-size: 1.25rem; margin: 0; padding: 0;">
+		<Icon name={getIconName(timeZone.tz)} time={getIconTime(timeZone.tz)} />
+		{timeZone.name}
+	</p>
+	<div style="display: flex; justify-content: space-between; gap: 1rem;">
+		<div>
+			<p>{timeZone.tz}</p>
+			<p>{getTZName(timeZone.tz)}</p>
+		</div>
+		<div>
+			<p>{getFullDate(timeZone.tz, currentTime)}</p>
+		</div>
+		<div>
+			<p>{getTime(timeZone.tz, currentTime)}</p>
+		</div>
+	</div>
 </div>
 
 <style>
-    /* Utility class */
-    .p-2 {
-        padding: 0.5rem;
-    }
+	/* Utility class */
+	.p-2 {
+		padding: 0.5rem;
+	}
 
-    .rounded-md {
-        border-radius: 0.375rem; /* 6px */
-    }
+	.rounded-md {
+		border-radius: 0.375rem; /* 6px */
+	}
 
-    .font-medium {
-        font-weight: 500;
-    }
+	.font-medium {
+		font-weight: 500;
+	}
 
-    /* Time utility class */
-    .timezone {
-        padding: 1rem;
-        margin-bottom: 1rem;
-    }
+	/* Time utility class */
+	.timezone {
+		padding: 1rem;
+		margin-bottom: 1rem;
+	}
 
-    .daytime {
-        background-color: #cce7ff; /* Light blue for day */
-        color: #000;
-    }
+	.daytime {
+		background-color: #cce7ff; /* Light blue for day */
+		color: #000;
+	}
 
-    .nighttime {
-        background-color: #2c3e50; /* Dark blue for night */
-        color: #fff;
-    }
+	.nighttime {
+		background-color: #2c3e50; /* Dark blue for night */
+		color: #fff;
+	}
 </style>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			'@icons': path.resolve(__dirname, 'src/assets/icons')
+		}
+	}
 });
